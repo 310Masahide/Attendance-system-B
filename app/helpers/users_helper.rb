8c35fa92283aab7e92e45d960ac4307b681b2e94
310Masahide <m55841029@gmail.com>
2026-07-15 04:21:31 +0900

    Finish add-user-attendances

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,14 @@
 module UsersHelper
 
-  # 勤怠基本情報を指定のフォーマットで返します。  
+  DAYS_OF_THE_WEEK = %w[日 月 火 水 木 金 土].freeze
+
+  # 勤怠基本情報を指定のフォーマットで返します。
   def format_basic_info(time)
     format("%.2f", ((time.hour * 60) + time.min) / 60.0)
+  end
+
+  # 日付に対応する曜日（日本語）を返します。
+  def day_of_week(date)
+    DAYS_OF_THE_WEEK[date.wday]
   end
 end
